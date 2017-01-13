294e5805bfa75d3e0b8053fa885ea178acbb9536
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2017-01-12 22:42:04 -0200

    add thread feature.

--- main.go
+++ main.go
@@ -24,102 +24,112 @@ type Stat struct {
 	Total            time.Duration
 }
 
-var nc int
+var c int
+var t int
+
+func init() {
+	flag.IntVar(&c, "c", 1, "Number of connections. It should be > 0.")
+	flag.IntVar(&t, "t", 1, "Total calls.")
+}
 
 func main() {
-	fs := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
-	fs.IntVar(&nc, "c", 1, "Number of connections. It should be > 0.")
-	fs.Usage = func() {
-		fmt.Printf("Usage: %s [OPTIONS] url\n", os.Args[0])
-		fs.PrintDefaults()
-	}
-	err := fs.Parse(os.Args[1:])
-	if err != nil {
-		os.Exit(2)
-	}
-	fsArgs := fs.Args()
-	if len(fsArgs) == 0 || nc == 0 {
-		fs.Usage()
+	flag.Parse()
+	fsArgs := flag.Args()
+	if len(fsArgs) == 0 {
 		os.Exit(1)
 	}
-	stats := []Stat{}
+	tc := t
+	nc := c
+	stats := make(chan Stat, tc*2)
 	url := fsArgs[0]
-	bench(url, nc, &stats)
+	bench(url, tc, nc, stats)
 	sumarize(stats, os.Stdout)
 	os.Exit(0)
 }
 
-func bench(url string, quantity int, stats *[]Stat) {
+func bench(url string, quantity int, threads int, stats chan Stat) {
 	var wg sync.WaitGroup
-	for i := 0; i < quantity; i++ {
+	for i := 0; i < threads; i++ {
 		wg.Add(1)
-		go visit(url, stats, &wg)
+		go visit(url, stats, quantity, &wg)
 	}
 	wg.Wait()
 }
 
-func visit(url string, stats *[]Stat, wg *sync.WaitGroup) {
-	var dnsStart, dnsDone, connDone, gotConn, transferInit, done time.Time
+func visit(url string, stats chan Stat, tc int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		log.Fatalf("new request failed: %v", err)
-	}
-	trace := &httptrace.ClientTrace{
-		DNSStart: func(_ httptrace.DNSStartInfo) { dnsStart = time.Now() },
-		DNSDone:  func(_ httptrace.DNSDoneInfo) { dnsDone = time.Now() },
-		ConnectStart: func(_, _ string) {
-			if dnsDone.IsZero() {
+	for {
+		var dnsStart, dnsDone, connDone, gotConn, transferInit, done time.Time
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			log.Fatalf("new request failed: %v", err)
+		}
+		trace := &httptrace.ClientTrace{
+			DNSStart: func(info httptrace.DNSStartInfo) {
+				dnsStart = time.Now()
+			},
+			DNSDone: func(info httptrace.DNSDoneInfo) {
 				dnsDone = time.Now()
-			}
-		},
-		ConnectDone: func(net, addr string, err error) {
-			if err != nil {
-				log.Fatalf("unable to connect to host %v: %v", addr, err)
-			}
-			connDone = time.Now()
-		},
-		GotConn:              func(_ httptrace.GotConnInfo) { gotConn = time.Now() },
-		GotFirstResponseByte: func() { transferInit = time.Now() },
+			},
+			ConnectStart: func(x, y string) {
+				if dnsDone.IsZero() {
+					dnsDone = time.Now()
+				}
+			},
+			ConnectDone: func(net, addr string, err error) {
+				if err != nil {
+					log.Fatalf("unable to connect to host %v: %v", addr, err)
+				}
+				connDone = time.Now()
+			},
+			GotConn: func(info httptrace.GotConnInfo) {
+				gotConn = time.Now()
+			},
+			GotFirstResponseByte: func() { transferInit = time.Now() },
+		}
+		req = req.WithContext(httptrace.WithClientTrace(context.Background(), trace))
+		tr := &http.Transport{
+			Proxy:                 http.ProxyFromEnvironment,
+			MaxIdleConns:          100,
+			IdleConnTimeout:       30 * time.Second,
+			TLSHandshakeTimeout:   10 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+		}
+		client := &http.Client{
+			Transport: tr,
+		}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatalf("request failed: %v", err)
+		}
+		fmt.Printf("%s\n", resp.Status)
+		done = time.Now()
+		if transferInit.IsZero() {
+			transferInit = done
+		}
+		if dnsStart.IsZero() {
+			dnsStart = dnsDone
+		}
+		stat := Stat{
+			DNSLookup:        dnsDone.Sub(dnsStart),
+			TCPConnection:    connDone.Sub(dnsDone),
+			TLSHandshake:     gotConn.Sub(connDone),
+			ServerProccesing: transferInit.Sub(gotConn),
+			ContentTransfer:  done.Sub(transferInit),
+			Total:            done.Sub(dnsStart),
+		}
+		stats <- stat
+		if len(stats) >= tc {
+			break
+		}
 	}
-	req = req.WithContext(httptrace.WithClientTrace(context.Background(), trace))
-	tr := &http.Transport{
-		Proxy:                 http.ProxyFromEnvironment,
-		MaxIdleConns:          100,
-		IdleConnTimeout:       30 * time.Second,
-		TLSHandshakeTimeout:   10 * time.Second,
-		ExpectContinueTimeout: 1 * time.Second,
-	}
-	client := &http.Client{
-		Transport: tr,
-	}
-	_, err = client.Do(req)
-	if err != nil {
-		log.Fatalf("request failed: %v", err)
-	}
-	done = time.Now()
-	if transferInit.IsZero() {
-		transferInit = done
-	}
-	if dnsStart.IsZero() {
-		dnsStart = dnsDone
-	}
-	stat := Stat{
-		DNSLookup:        dnsDone.Sub(dnsStart),
-		TCPConnection:    connDone.Sub(dnsDone),
-		TLSHandshake:     gotConn.Sub(connDone),
-		ServerProccesing: transferInit.Sub(gotConn),
-		ContentTransfer:  done.Sub(transferInit),
-		Total:            done.Sub(dnsStart),
-	}
-	iStats := *stats
-	iStats = append(iStats, stat)
-	*stats = iStats
 }
 
-func sumarize(stats []Stat, w io.Writer) {
+func sumarize(stats chan Stat, w io.Writer) {
 	summ := Stat{}
-	for _, s := range stats {
+	size := int64(len(stats))
+	close(stats)
+	for s := range stats {
 		summ.DNSLookup = time.Duration(summ.DNSLookup.Nanoseconds() + s.DNSLookup.Nanoseconds())
 		summ.TCPConnection = time.Duration(summ.TCPConnection.Nanoseconds() + s.TCPConnection.Nanoseconds())
 		summ.TLSHandshake = time.Duration(summ.TLSHandshake.Nanoseconds() + s.TLSHandshake.Nanoseconds())
@@ -127,12 +137,12 @@ func sumarize(stats []Stat, w io.Writer) {
 		summ.ContentTransfer = time.Duration(summ.ContentTransfer.Nanoseconds() + s.ContentTransfer.Nanoseconds())
 		summ.Total = time.Duration(summ.Total.Nanoseconds() + s.Total.Nanoseconds())
 	}
-	summ.DNSLookup = time.Duration((summ.DNSLookup.Nanoseconds() / int64(len(stats))))
-	summ.TCPConnection = time.Duration((summ.TCPConnection.Nanoseconds() / int64(len(stats))))
-	summ.TLSHandshake = time.Duration((summ.TLSHandshake.Nanoseconds() / int64(len(stats))))
-	summ.ServerProccesing = time.Duration((summ.ServerProccesing.Nanoseconds() / int64(len(stats))))
-	summ.ContentTransfer = time.Duration((summ.ContentTransfer.Nanoseconds() / int64(len(stats))))
-	summ.Total = time.Duration((summ.Total.Nanoseconds() / int64(len(stats))))
+	summ.DNSLookup = time.Duration((summ.DNSLookup.Nanoseconds() / size))
+	summ.TCPConnection = time.Duration((summ.TCPConnection.Nanoseconds() / size))
+	summ.TLSHandshake = time.Duration((summ.TLSHandshake.Nanoseconds() / size))
+	summ.ServerProccesing = time.Duration((summ.ServerProccesing.Nanoseconds() / size))
+	summ.ContentTransfer = time.Duration((summ.ContentTransfer.Nanoseconds() / size))
+	summ.Total = time.Duration((summ.Total.Nanoseconds() / size))
 	sumaryTmpl := `Average request time: {{.Total.Seconds }}s
 DNS Lookup: {{ .DNSLookup.Seconds }}s
 TCP Connections: {{ .TCPConnection.Seconds }}s
